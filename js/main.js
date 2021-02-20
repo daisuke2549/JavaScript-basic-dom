51d6dc4aaedc275db9a228693039509ebd446a29
daisuke sasaki <gerund25495641@gmail.com>
2021-02-20 14:58:01 +0900

    カスタム属性を扱ってみようまで終了

--- js/main.js
+++ js/main.js
@@ -1,16 +1,58 @@
+// 'use strict';
+
+// { 
+//   document.querySelector('button').addEventListener('click', () => {
+//     document.getElementById('test').textContent = 'Changed!';
+//   });
+// }
+
+// {
+//    function update(){
+//    //    document.querySelector('#target').textContent = 'changed';
+//    //    document.getElementById('test').textContent = 'changed';
+//    //    document.querySelector('p').textContent = 'テキスト変更'; 
+//    //    document.querySelectorAll('p')[0,1,2].textContent = 'テキスト変更'; 
+
+//    document.querySelectorAll('p').forEach((p, index) => {
+//        p.textContent = 'テキスト変更'; 
+//    });
+//    }
+
+//    setTimeout(update, 1900);
+// }
+
+
+
+// 'use strict';
+// { 
+//   document.querySelector('button').addEventListener('click', () => {
+//        const targetNode = document.getElementById('test');
+//     //    targetNode.className = 'my-color my-border';
+
+//     // // targetNode.classList.add('my-color');
+//     // // targetNode.classList.add('my-border');
+//     targetNode.classList.toggle('my-color');
+
+//   });
+// }
+
+
+
 'use strict';
+{ 
+  document.querySelector('button').addEventListener('click', () => {
+    const targetNode = document.getElementById('test');
 
-{
-   function update(){
-   //    document.querySelector('#target').textContent = 'changed';
-   //    document.getElementById('test').textContent = 'changed';
-   //    document.querySelector('p').textContent = 'テキスト変更'; 
-   //    document.querySelectorAll('p')[0,1,2].textContent = 'テキスト変更'; 
+    targetNode.textContent = 'Dotinstall';
+    targetNode.classList.toggle('my-color');
+    targetNode.classList.toggle('my-border');
 
-   document.querySelectorAll('p').forEach((p, index) => {
-       p.textContent = 'テキスト変更'; 
-   });
-   }
+    targetNode.textContent = targetNode.dataset.translation;
 
-   setTimeout(update, 1900);
+  });
 }
+
+
+
+
+
