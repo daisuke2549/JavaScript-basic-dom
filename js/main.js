28ffed25fc1a49bdfe6870633107724a88979a50
daisuke sasaki <gerund25495641@gmail.com>
2021-02-20 15:50:42 +0900

    dom basic complete

--- js/main.js
+++ js/main.js
@@ -56,18 +56,102 @@
 
 
 
-{ 
-  document.querySelector('button').addEventListener('click', () => {
-    const item2 = document.createElement('li');
-    item2.textContent = 'item2';
+// { 
+//   document.querySelector('button').addEventListener('click', () => {
+//     const item2 = document.createElement('li');
+//     item2.textContent = 'item2';
 
 
-    const ul = document.querySelector('ul');
-    ul.appendChild(item2);
-  }); 
+//     const ul = document.querySelector('ul');
+//     ul.appendChild(item2);
+//   }); 
+// }
+
+
+
+
+// 'use strict';
+
+// {
+//   document.querySelector('button').addEventListener('click', () => {
+//        const item1 = document.querySelectorAll('li')[1];
+
+//        document.querySelector('ul').removeChild(item1);
+//   });
+// }
+
+
+
+// 'use strict';
+
+// {
+//   document.querySelector('button').addEventListener('click', () => {
+//     const li = document.createElement('li');
+//     const color = document.querySelector('select');
+//     li.textContent = `${color.value} - ${color.selectedIndex}`;
+//     document.querySelector('ul').appendChild(li);
+//   });
+// }
+
+
+
+// 'use strict';
+
+// {
+//   document.querySelector('button').addEventListener('dblclick', () => {
+//     console.log('Double Clicked!');
+//   });
+
+//   document.addEventListener('mousemove', () => {
+//     console.log('moved!');
+//   });
+// }
+
+
+// 'use strict';
+
+// {
+//   const text = document.querySelector('textarea')
+
+// //   text.addEventListener('focus', () => {
+// //       console.log('focus');
+
+// //   });
+// //   text.addEventListener('blur', () => {
+// //     console.log('blur');
+// // });
+
+
+// text.addEventListener('input', () => {
+//     console.log(text.value.length);
+
+// });
+// text.addEventListener('change', () => {
+//   console.log('change');
+// });
+
+
+// }
+
+
+// 'use strict';
+
+// {
+//   document.querySelector('ul').addEventListener('click', e => {
+//     if (e.target.nodeName === 'LI') {
+//         e.target.classList.toggle('done');
+//     }
+//   });
+// }
+
+
+'use strict'
+
+/* strict モードを使うとコードが厳格モードで実行されるようになる*/
+
+for (var i = 1; i <=111111110; i++){
+    console.log(i);
 }
-
-
 
 
 
