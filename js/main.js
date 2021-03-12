27776d80ec9d391d77cdfc212db6774552eb239b
daisuke sasaki <gerund25495641@gmail.com>
2021-03-12 23:39:05 +0900

    test

--- js/main.js
+++ js/main.js
@@ -1,157 +1,158 @@
-// 'use strict';
-
-// { 
-//   document.querySelector('button').addEventListener('click', () => {
-//     document.getElementById('test').textContent = 'Changed!';
-//   });
-// }
-
-// {
-//    function update(){
-//    //    document.querySelector('#target').textContent = 'changed';
-//    //    document.getElementById('test').textContent = 'changed';
-//    //    document.querySelector('p').textContent = 'テキスト変更'; 
-//    //    document.querySelectorAll('p')[0,1,2].textContent = 'テキスト変更'; 
-
-//    document.querySelectorAll('p').forEach((p, index) => {
-//        p.textContent = 'テキスト変更'; 
-//    });
-//    }
-
-//    setTimeout(update, 1900);
-// }
+// alert(document.getElementById('js-for-web').outerHTML)
 
 
+// alert(document.getElementsByClassName('lead')[0].outerHTML)
+// alert(document.getElementsByClassName('lead')[1].outerHTML)
 
-// 'use strict';
-// { 
-//   document.querySelector('button').addEventListener('click', () => {
-//        const targetNode = document.getElementById('test');
-//     //    targetNode.className = 'my-color my-border';
 
-//     // // targetNode.classList.add('my-color');
-//     // // targetNode.classList.add('my-border');
-//     targetNode.classList.toggle('my-color');
+// alert( document.getElementsByTagName('a')[0].outerHTML )
 
-//   });
-// }
+
+// document.getElementById("service-title").innerText ="ディトラで勉強しよう";
+
+
+// const login_btn = document.createElement('a');
+
+
+// login_btn.innerText  = 'ログイン';
+// login_btn.setAttribute('id', 'login')
+// login_btn.setAttribute('class', 'btn')
+// login_btn.setAttribute('href', 'https://tokyofreelance.jp/')
+// document.getElementById('js-for-web').appendChild(login_btn)
+
+// document.getElementById("register").click()
+
+
+// document.getElementById("button").addEventListener('click', function() {
+//     document.getElementById('service-title').innerText = 'デイトラ' 
+//   })  
+//  window.onload = function() {
+//     document.getElementById('service-title').innerText = 'デイトラ' 
+// } 
 
 
 
-// 'use strict';
-// { 
-//   document.querySelector('button').addEventListener('click', () => {
-//     const targetNode = document.getElementById('test');
+// $(function(){
 
-//     targetNode.textContent = 'Dotinstall';
-//     targetNode.classList.toggle('my-color');
-//     targetNode.classList.toggle('my-border');
-
-//     targetNode.textContent = targetNode.dataset.translation;
-
-//   });
-// }
+//     $(".area").hide();
 
 
 
+//     $("#btn1").on("click", function(){
+//         $("#area").show();
+//     });
+//     $("#btn2").on("click", function(){
+//         $("#area").hide();
+//     });
 
 
-// { 
-//   document.querySelector('button').addEventListener('click', () => {
-//     const item2 = document.createElement('li');
-//     item2.textContent = 'item2';
+//     $("#alert-button").on("click", function(){
+//         alert("ボタンがクリックされました");
+//     });
 
-
-//     const ul = document.querySelector('ul');
-//     ul.appendChild(item2);
-//   }); 
-// }
-
-
-
-
-// 'use strict';
-
-// {
-//   document.querySelector('button').addEventListener('click', () => {
-//        const item1 = document.querySelectorAll('li')[1];
-
-//        document.querySelector('ul').removeChild(item1);
-//   });
-// }
-
-
-
-// 'use strict';
-
-// {
-//   document.querySelector('button').addEventListener('click', () => {
-//     const li = document.createElement('li');
-//     const color = document.querySelector('select');
-//     li.textContent = `${color.value} - ${color.selectedIndex}`;
-//     document.querySelector('ul').appendChild(li);
-//   });
-// }
-
-
-
-// 'use strict';
-
-// {
-//   document.querySelector('button').addEventListener('dblclick', () => {
-//     console.log('Double Clicked!');
-//   });
-
-//   document.addEventListener('mousemove', () => {
-//     console.log('moved!');
-//   });
-// }
-
-
-// 'use strict';
-
-// {
-//   const text = document.querySelector('textarea')
-
-// //   text.addEventListener('focus', () => {
-// //       console.log('focus');
-
-// //   });
-// //   text.addEventListener('blur', () => {
-// //     console.log('blur');
-// // });
-
-
-// text.addEventListener('input', () => {
-//     console.log(text.value.length);
+//     $('.title').addClass('color-class');
+//     $('.title').removeClass('color-class');
 
 // });
-// text.addEventListener('change', () => {
-//   console.log('change');
+
+// for (i = 0; i < 10; i++) {
+//     $('.myClass').append('<li>Item' + i + '</li>');  
+// }
+
+
+// $(function(){
+//    $("#sample").append("注意");
 // });
 
 
-// }
-
-
-// 'use strict';
-
-// {
-//   document.querySelector('ul').addEventListener('click', e => {
-//     if (e.target.nodeName === 'LI') {
-//         e.target.classList.toggle('done');
-//     }
+// $(function(){
+//     $("#sample3").remove();
 //   });
+
+
+
+// $(function(){
+//     $("h2").text("h2のテキストを変更")
+// })
+
+// $('#button').click(function(){
+//     $('#service-title').text('ディトラ');
+// });
+
+
+
+
+// $(function(){
+//     $("a").hide();
+// })
+
+
+
+// let name = "suzuki";
+
+// console.log(name);
+
+// name = "yamagata";
+
+// console.log(name);
+
+
+// const human = "human";
+
+// console.log(human);
+
+
+
+// let str = "important subject is mathmatics";
+// console.log(str.length);
+
+
+// for ( let i = 0;  i <= 100; i++){
+
+// if(i % 15 === 0) {
+//     console.log('FizzBuzz')
+// }else if(i % 5 === 0){
+//     console.log('Fizz')
+// }else if(i % 3 === 0){
+//     console.log('Buzz')
+// }else{
+//     console.log(i);
+// }
 // }
 
+for (var i = 0; i < 10;  i++){
+    console.log(i+10);
+}
 
-'use strict'
+let lists = ['佐藤', '伊藤', '鈴木', '木村', '田中', '高橋'];
 
-/* strict モードを使うとコードが厳格モードで実行されるようになる*/
-
-for (var i = 1; i <=10; i++){
-    console.log(i);
+for ( var i = 0; i < lists.length; i++){
+    console.log(lists[i]);
 }
 
 
+for (let i = 0; i <= 10; i++){
+   if ( i  === 7){
+       break;
+   }
+    console.log(i);
 
+}
+
+
+let scores = [10, 20, 30, 40, 50];
+
+scores.push(60);
+
+
+console.log(scores);
+
+scores.shift();
+
+console.log(scores);
+
+
+console.log(scores.length);
+
+
+console.log(scores[1]);
